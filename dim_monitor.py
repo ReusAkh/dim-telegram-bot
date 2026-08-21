485f75733af802be7c260f2454d26a87df35def5
ReusAkh <164777425+ReusAkh@users.noreply.github.com>
2026-08-21 17:57:52 +0400

    Create dim_monitor.py

---
+++ dim_monitor.py
@@ -0,0 +1,17 @@
+import requests
+
+DIM_URL = "https://exidmet.dim.gov.az/dqq/ImtQeyd
+
+response = requests.get(
+  DIM_URL,
+  timeout=30
+  headers={
+      "User-Agent": "Mozilla/5.0"
+ }
+)
+
+print("Status:", response.status_code)
+print("Səhifənin ölçüsü:", len(response.text))
+
+print("\n--- Səhifənin ilk 1000 simvolu ---\n")
+print(response.text[:1000])
