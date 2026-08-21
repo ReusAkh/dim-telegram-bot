c616ad5ca18eee605d2df7e028282b8cec8d3f5a
ReusAkh <164777425+ReusAkh@users.noreply.github.com>
2026-08-21 18:16:31 +0400

    Update dim_monitor.py

--- dim_monitor.py
+++ dim_monitor.py
@@ -1,4 +1,8 @@
 import requests
+import urllib3
+
+# Test məqsədilə SSL xəbərdarlığını gizlədirik
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 DIM_URL = "https://exidmet.dim.gov.az/dqq/ImtQeyd"
 
@@ -7,11 +11,12 @@ response = requests.get(
     timeout=30,
     headers={
         "User-Agent": "Mozilla/5.0"
-    }
+    },
+    verify=False
 )
 
 print("Status:", response.status_code)
 print("Səhifənin ölçüsü:", len(response.text))
 
-print("\n--- Səhifənin ilk 1000 simvolu ---\n")
-print(response.text[:1000])
+print("\n--- Səhifənin ilk 2000 simvolu ---\n")
+print(response.text[:2000])
